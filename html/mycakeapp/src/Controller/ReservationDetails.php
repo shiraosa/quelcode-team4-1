727609ae9e85fa04c4167056ed0bb014b0221a5b
nanakusa27 <masakiw27@yahoo.co.jp>
2020-11-16 10:52:34 +0900

    Add:ファイルの追加（空）

---
+++ html/mycakeapp/src/Controller/ReservationDetails.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\ORM\Table;
+use Cake\Event\Event;
+
+class ToppageController extends CinemabaseController
+{
+    public function index()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function deleted()
+    {
+    }
+}
