570a7badeb963df795f9875dce9e414d19923297
shiraosa <tezukatrigger@gmail.com>
2020-11-24 14:46:04 +0900

    Add:Layout.jsを使用

---
+++ html/mycakeapp/webroot/js/seatsReservations.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    function loadGrid() {
+
+        $('.selectMove').seatLayout({
+            data: seatData,
+            showActionButtons: true,
+            classes: {
+                doneBtn: '',
+                cancelBtn: '',
+                row: '',
+                area: '',
+                screen: '',
+                seat: ''
+            },
+            numberOfSeat: 1,
+            callOnSeatRender: function (Obj) {
+                //modify seat object if require and return it;
+                return Obj;
+            },
+            callOnSeatSelect: function (_event, _data, _selected, _element) {
+                console.log(_event);
+                console.log(_data);
+                console.log(_selected);
+            },
+            selectionDone: function (_array) {
+
+                console.log(_array);
+            },
+            cancel: function () {
+                return false;
+            }
+        });
+    }
+    loadGrid();
+
+});
