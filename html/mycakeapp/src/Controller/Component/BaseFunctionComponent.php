2ace073c1b4c6590c8f15341d4ae28e6e0f1eb6c
nanakusa27 <masakiw27@yahoo.co.jp>
2020-11-26 18:08:09 +0900

    Add:使用可能なクレジットカード情報取得を関数化Fix:deleteSessionの論理破綻回収

--- html/mycakeapp/src/Controller/Component/BaseFunctionComponent.php
+++ html/mycakeapp/src/Controller/Component/BaseFunctionComponent.php
@@ -4,31 +4,51 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\ORM\TableRegistry;
+use Cake\I18n\Time;
 
 class BaseFunctionComponent extends Component
 {
-    public function destroySessionReservation($session)
+    /**
+     * Undocumented function
+     * 一連の予約処理で使われる各セッションの削除
+     *
+     * @param [type] $session $this->request->getSession()したものを引数とする
+     * @return void なし
+     */
+    public function deleteSessionReservation($session)
     {
-        if ($session->check('schedule')) {
-            $session->delete('schedule');
-        }
-        if ($session->check('profile')) {
-            $session->delete('profile');
-        }
-        if ($session->check('discountTypeId')) {
-            $session->delete('discountTypeId');
-        }
-        if ($session->check('usePoint')) {
-            $session->delete('usePoint');
-        }
-        if ($session->check('totalPayment')) {
-            $session->delete('totalPayment');
-        }
+        $session->delete('schedule');
+        $session->delete('profile');
+        $session->delete('discountTypeId');
+        $session->delete('price');
+        $session->delete('creditcard');
+        $session->delete('usePoint');
+        $session->delete('totalPayment');
 
         // 座席テーブルに削除フラグを立てる
-        $seatsTable = TableRegistry::getTableLocator()->get('Seat');
-        $seat = $seatsTable->get($session->read('seatId'));
-        $seat->is_deleted = 1;
-        $seatsTable->save($seat);
+        if (($session->check('seat_id')) && (($session->read('completed') === null))) {
+            $seatsTable = TableRegistry::getTableLocator()->get('Seats');
+            $seat = $seatsTable->get($session->read('seat_id'));
+            $seat->is_deleted = 1;
+            $seatsTable->save($seat);
+
+            $session->delete('completed');
+            $session->delete('seat_id');
+        }
+    }
+
+    /**
+     * Undocumented function
+     * 認証ユーザーの使用可能なクレジットカード情報を検索します
+     *
+     * @param [type] $userId 認証ユーザーIDを引数とする
+     * @return void 使用可能なクレジットカード情報 || null
+     */
+    public function aliveCreditcard($userId) {
+        $creditcards = TableRegistry::getTableLocator()->get('Creditcards');
+        $aliveCredit = $creditcards->find('all', [
+            'conditions' => ['AND' => [['user_id' => $userId], ['is_deleted' => 0], ['expiration_date >=' => Time::now()->year . '-' . Time::now()->month . '-1']]]
+        ])->first();
+        return $aliveCredit;
     }
 }
