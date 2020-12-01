4d62a2ede49ad20927272074b8f3dcda95525c6f
RyomaY <docomoknps200uu@gmail.com>
2020-12-01 09:31:06 +0900

    Add: DaysComponentにphpDocを追加

--- html/mycakeapp/src/Controller/Component/DaysComponent.php
+++ html/mycakeapp/src/Controller/Component/DaysComponent.php
@@ -4,8 +4,20 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 
+/**
+ * 曜日の関数
+ */
+
 class DaysComponent extends Component
 {
+    /**
+     * dateObjectをstrに変換する
+     *
+     * m月d日(曜日)を出力する
+     * 
+     * @param obj $day
+     * @return str $day
+     */
     public function __getDayOfTheWeek($day)
     {
         $weekday = ['日', '月', '火', '水', '木', '金', '土'];
