06e55d879ec42bf2c5a644b0c47476ab99ceb5a5
RyomaY <docomoknps200uu@gmail.com>
2020-12-29 09:45:41 +0900

    Add: ErrorLevel

--- html/mycakeapp/config/app.php
+++ html/mycakeapp/config/app.php
@@ -177,7 +177,7 @@ return [
      *   breathing room to complete logging or error handling.
      */
     'Error' => [
-        'errorLevel' => E_ALL,
+        'errorLevel' => env('ERROR_LEVEL', E_ALL),
         'exceptionRenderer' => ExceptionRenderer::class,
         'skipLog' => [],
         'log' => true,
